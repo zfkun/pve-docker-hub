76c0f67e4093b9b360a135103056da3003340bff
zfkun <zfkun@msn.com>
2025-04-07 13:52:34 +0800

    feat: add godns

---
+++ godns/build.sh
@@ -0,0 +1,9 @@
+IMAGE_NAME=zfkun/amd64-pve-godns
+GODNS_VERSION=3.2.3
+GODNS_REPO=https://ghfast.top/https://github.com/TimothyYe/godns
+
+# build latest
+docker build --network host --build-arg GODNS_VERSION=$GODNS_VERSION --build-arg GODNS_REPO=$GODNS_REPO -t $IMAGE_NAME:latest
+
+# build specific version
+docker tag $IMAGE_NAME:latest $IMAGE_NAME:$GODNS_VERSION
